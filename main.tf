693e17d167102eae8373209001d571008bec519b
Axel Ramirez <axel.ramirez@uipath.com>
2020-12-17 15:32:35 -0800

    initial commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-04d29b6f966df1537"
+  instance_type = "t2.micro"
+
+    tags = {
+    Name = "terraform-example"
+    Owner = "axel.ramirez@uipath.com"
+  }
+}
